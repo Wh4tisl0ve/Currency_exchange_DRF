4b3eca8d2c9a4a060aafab774999ce9c60419538
Владислав <95215503+Wh4tisl0ve@users.noreply.github.com>
2024-12-09 00:15:21 +0300

    Update views.py

--- src/currency_exchange_project/currencies/views.py
+++ src/currency_exchange_project/currencies/views.py
@@ -8,10 +8,7 @@ from .serializers import CurrencySerializer, CurrencyWriteSerializer
 class CurrenciesViewSet(viewsets.ModelViewSet):
     serializer_class = CurrencySerializer
     queryset = Currency.objects.all()
-<<<<<<< HEAD
     lookup_field = 'code'
-=======
->>>>>>> 7db29b10acc09ad309658932a28b13d18edd747a
 
     def get_serializer_class(self):
         if self.action == "create":
@@ -29,11 +26,7 @@ class CurrenciesViewSet(viewsets.ModelViewSet):
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-<<<<<<< HEAD
     def retrieve(self, request, code):
-=======
-    def retrieve(self, request, pk):
->>>>>>> 7db29b10acc09ad309658932a28b13d18edd747a
         currency = self.get_object()
         serializer = self.get_serializer(currency)
         return Response(serializer.data)
